e81e9058fbe04a9b544e404e03540ecb0bbb5691
lokesh26 <lokesh.agg26@gmail.com>
2022-07-08 08:13:07 +0200

    Redirecting unknown path to root with message

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,6 +1,11 @@
 class AccountsController < ApplicationController
 	before_action :authenticate_user!
-	
+
+	def unknown
+		flash.notice = 'No page found at that address'
+		redirect_to root_path
+	end
+
 	def show
 	end
 end
